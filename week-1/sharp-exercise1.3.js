54079053363db6d3f0b94a307cf7122cba9e1312
Robert Sharp <robert.sharp.dev@gmail.com>
2018-02-25 17:43:33 -0600

    Adding week 1 folder and header

---
+++ week-1/sharp-exercise1.3.js
@@ -0,0 +1,14 @@
+console.log(header.display("Robert", "Sharp", "Excercise 1.3"));
+console.log("\n");
+//referenced header.js file above
+
+var url = require('url');
+
+var parsedURL = url.parse('https://www.123example.org/profile?name=sharp');
+
+/*the following console.log()'s log the protocol, host,
+and query respectively */
+
+console.log(parsedURL.protocol); 
+console.log(parsedURL.host);
+console.log(parsedURL.query);
