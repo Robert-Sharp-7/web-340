98ecb44efb2f76c8f27deb52d81b783040b21983
Robert Sharp <robert.sharp.dev@gmail.com>
2018-04-15 21:09:24 -0500

    Adding week-8

--- ems/app.js
+++ ems/app.js
@@ -1,17 +1,41 @@
+//app.js assignment for Web 340 Milestone assignments
+//author: Professor Krasso
+//modified by: Robert Sharp
+
+//requirement statements
 var express = require("express");
 var http = require("http");
 var path = require("path");
 var logger = require("morgan");
+var mongoose = require("mongoose");
+var helmet = require("helmet");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var csrf = require("csurf");
 
+
+//set up csurf protection
+var csrfProtection = csrf({cookie: true});
+
+//initializes express
 var app = express();
 
-// defines the employee model
-var Employee = mongoose.model("Employee", employeeSchema);
-
 var mongoDB = "mongodb://robert.sharp:password@ds159180.mlab.com:59180/ems"
+mongoose.connect(mongoDB, {
+    useMongoClient: true
+});
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+db.once("open", function() {
+    console.log("Application connected to mLab MongoDB instance");
+});
 //Mongo databse connection string
 //utilizes my username: robert.sharp
 //and password: password
+
 
 app.set("views", path.resolve(__dirname, "views"));
 //this says that the views are in the views directory
@@ -19,13 +43,48 @@ app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 //this tells the application to view it using ejs
 
+//use statements
 app.use(logger("short"));
+app.use(helmet.xssFilter());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cookieParser());
+app.use(csrfProtection);
 
-
+app.use(function(request, response, next){
+    var token = request.csrfToken();
+    response.cookie('XSRF-TOKEN', token);
+    response.locals.csrfToken = token;
+    next();
+});
+//http calls
 app.get("/", function(request, response){
     response.render("index",{
-        title: "HOME PAGE"
+        title: "HOME PAGE",
     });
+});
+
+app.get("/new", function (request, response){
+    response.render("new", {
+        title: "This is the New Page"
+    })
+})
+
+app.get("/list", function(request, response){
+    entry.find({}, function(error, fruits){
+        if(error) throw error;
+
+        response.render("list", {
+            title: "Entry List",
+            data: entries
+        });
+    });
+});
+
+app.post("/process", function(request, response){
+    console.log(request.body.txtName);
+    response.redirect("/");
 });
 //the get function has the parameters of request and response,
 //and the response is to render the index page with the title of "HOME PAGE"
